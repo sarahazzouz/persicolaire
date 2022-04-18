33eaf883f07d168e507795343eba921b4b5c934b
sarahazzouz <99666637+sarahazzouz@users.noreply.github.com>
2022-04-18 13:17:17 +0200

    Update Enfant.java

--- src/Enfant.java
+++ src/Enfant.java
@@ -1,39 +1,21 @@
-
-import java.util.*;
-
-/**
- * 
- */
-public class Enfant extends Parent {
-
-    /**
-     * Default constructor
-     */
-    public Enfant() {
+public class Enfant extends Personne{
+    private int age ; 
+    private String niveaux_S;
+    public Enfant(String nom , String prenom , String address , int age ,String niveaux_S){
+        super(nom , prenom , address);
+        this.age = age ; 
+        this.niveaux_S= niveaux_S;
     }
-
-    /**
-     * 
-     */
-    public void Attribute1;
-
-    /**
-     * 
-     */
-    public void Attribute2;
-
-    /**
-     * 
-     */
-    public void int age;
-
-    /**
-     * 
-     */
-    public void String niveau-S;
-
-
-
-
-
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setNiveaux_S(String niveaux_S) {
+        this.niveaux_S = niveaux_S;
+    }
+    public String getNiveaux_S() {
+        return niveaux_S;
+    }
+    public int getAge() {
+        return age;
+    }
 }
